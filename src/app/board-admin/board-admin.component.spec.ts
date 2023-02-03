ea609f60ae28d17c66f1650ceb648d76ac8dedba
francescafiorentino <francescafiorentino10@gmail.com>
2023-02-03 09:34:04 +0100

    prova commit visual studio code

---
+++ src/app/board-admin/board-admin.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoardAdminComponent } from './board-admin.component';
+
+describe('BoardAdminComponent', () => {
+  //----------------COMMENTO-------
+  let component: BoardAdminComponent;
+  let fixture: ComponentFixture<BoardAdminComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BoardAdminComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BoardAdminComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
